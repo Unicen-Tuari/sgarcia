b79a4622156157ff0ddc38a1dafae64f908c3472
SergioGarciaRetegui <garcia.r.sergio@gmail.com>
2016-04-20 23:19:03 -0300

    Estructura de trabajo especial javascript con notas de guia

---
+++ Ruleta/js/ruleta.js
@@ -0,0 +1,34 @@
+"use strict";
+var saldo=100;
+var numeroApuesta:-1;
+function descontarSaldo(){
+//esta funcion deberia descontar saldo del apostador
+//debe utilizar:
+//document.getElementById(id_saldo)-=valor a descontar
+//de esta manera actualizamos el valir de la pantalla
+}
+
+function acreditarSaldo(){
+//esta funcion deberia descontar saldo del apostador
+//debe utilizar:
+//document.getElementById(id_saldo)+=valor a sumar
+//de esta manera actualizamos el valir de la pantalla
+}
+function generarJugada(){
+//return Math.floor((Math.random() * 6)
+  //esta funcion me genera un valor de 0 a 9 (10 posibles valores)
+}
+function Apostar(){
+
+  // llama al generador de numeroApuesta
+  //consulta por el valor ingresado y el valor salido
+  // muestra el resultado -- estaria bueno agregar una imagen con el numero salido
+  // llamar a la funccion que acredita o quita saldo
+}
+function tirarRuleta(){
+// guardo el valor apostado e una variable
+// la comparo con la apuesta minima (validar) hacer la condicion para que entre por falso
+//  si da error tirar un alert - cerrar el camino con un return;
+// si es correcto seguir el hilo de la ejecucion y llamar a la funcion tirarApuesta
+alert("hace algo");
+}
