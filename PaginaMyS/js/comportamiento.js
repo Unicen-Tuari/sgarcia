2a5f3bf9d91877d2d6da8ff3e38b00b73f912038
SergioGarciaRetegui <garcia.r.sergio@gmail.com>
2016-06-29 11:28:36 -0300

    Update comportamiento.js

--- PaginaMyS/js/comportamiento.js
+++ PaginaMyS/js/comportamiento.js
@@ -20,10 +20,10 @@ function cargaretiqueta(etiqueta) {
           }
         );
   }
-  $("#empresa").on("click", function(){cargaretiqueta("../HTML/empresa.html")});
-  $("#maquinas").on("click", function(){cargaretiqueta("../HTML/maquinas.html")});
-  $("#embutidoras").on("click", function(){cargaretiqueta("../HTML/embutidora.html")});
-  $("#mixer").on("click", function(){cargaretiqueta("../HTML/mixers.html")});
-  $("#moledoras").on("click", function(){cargaretiqueta("../HTML/moledora.html")});
-  $("#contacto").on("click", function(){cargaretiqueta("../HTML/contacto.html")});
+  $("#empresa").on("click", function(){cargaretiqueta("HTML/empresa.html")});
+  $("#maquinas").on("click", function(){cargaretiqueta("HTML/maquinas.html")});
+  $("#embutidoras").on("click", function(){cargaretiqueta("HTML/embutidora.html")});
+  $("#mixer").on("click", function(){cargaretiqueta("HTML/mixers.html")});
+  $("#moledoras").on("click", function(){cargaretiqueta("HTML/moledora.html")});
+  $("#contacto").on("click", function(){cargaretiqueta("HTML/contacto.html")});
 });
