92f24647914fe57a0cc4fb3d5e46caa1c2e10712
SergioGarciaRetegui <garcia.r.sergio@gmail.com>
2016-06-29 01:26:07 -0300

    prueba

--- PaginaMyS/js/comportamiento.js
+++ PaginaMyS/js/comportamiento.js
@@ -2,16 +2,16 @@
 $("#hom").on("click", handleClick);
 function handleClick() {
     $("#contenido").html("<h1>Loading 1</h1>");
-    $.ajax("https://paginamys.herokuapp.com/PaginaMyS?",
+    $.ajax("https://paginamys.herokuapp.com/PaginaMyS/HTML",
           {
             error: function () {
               alert("no cargo");
             },
             dataType: "HTML",
             success: function (receivedData) {
-              $("#ajaxContent").html(receivedData);
+              $("#contenido").html(receivedData);
               //console.log("Lenght: "+ $(".js-comportamiento").length);
-              $(".js-comportamiento").html("Borrar");
+              //$(".js-comportamiento").html("Borrar");
             },
             method: "GET"
           }
