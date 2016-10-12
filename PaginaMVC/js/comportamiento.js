ad2fc3e0b6d72f9133a7838529a3987dfbff68b0
SergioGarciaRetegui <garcia.r.sergio@gmail.com>
2016-10-12 18:49:00 -0300

    agregue los prevent default en el evento onclick ya que me generaba conflicto al cargar la pagina (bucle)

--- PaginaMVC/js/comportamiento.js
+++ PaginaMVC/js/comportamiento.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 cargaretiqueta("index.php?action=mostrar_home");
 
 function cargaretiqueta(etiqueta) {
-   event.preventDefault();
+  // event.preventDefault();
     $.ajax({
             method: "GET",
             url:etiqueta,
@@ -20,13 +20,22 @@ function cargaretiqueta(etiqueta) {
           }
         );
   }
-  $("#home").on("click", function(){cargaretiqueta("index.php?action=mostrar_home")});
-  $("#empresa").on("click", function(){cargaretiqueta("index.php?action=mostrar_nosotros")});
+  $("#home").on("click", function(e){
+    e.preventDefault();
+    cargaretiqueta("index.php?action=mostrar_home")
+  }
+  );
+  $("#empresa").on("click", function(e){
+    e.preventDefault();
+    cargaretiqueta("index.php?action=mostrar_nosotros")}
+  );
   //$("#maquinas").on("click", function(){cargaretiqueta("templates/maquinas.tpl")});
   //$("#embutidoras").on("click", function(){cargaretiqueta("templates/embutidora.tpl")});
   //$("#mixer").on("click", function(){cargaretiqueta("templates/mixers.tpl")});
   //$("#moledoras").on("click", function(){cargaretiqueta("templates/moledora.tpl")});
-  $("#contacto").on("click", function(){cargaretiqueta("index.php?action=formulario_contacto")});
+  $("#contacto").on("click", function(e){
+    e.preventDefault();
+    cargaretiqueta("index.php?action=formulario_contacto")});
   //$("#tablas").on("click", function(){cargaretiqueta("templates/tecnica.tpl")});
 
 });
